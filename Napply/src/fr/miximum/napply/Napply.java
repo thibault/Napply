34168100b34ceb28e4ef4be43c06f2ffece60587
Thibault Jouannic <teeboo@gmail.com>
2011-02-24 18:15:21 +0100

    Forgot to commit new actions

--- Napply/src/fr/miximum/napply/Napply.java
+++ Napply/src/fr/miximum/napply/Napply.java
@@ -13,6 +13,10 @@ public class Napply extends Activity
 
     public static final String ACTION_CANCEL_ALARM = "fr.miximum.napply.CANCEL_ALARM";
 
+    public static final String ACTION_SNOOZE_ALARM = "fr.miximum.napply.SNOOZE_ALARM";
+
+    public static final String ACTION_DESTROY_DIALOG = "fr.miximum.napply.DESTROY_DIALOG";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)

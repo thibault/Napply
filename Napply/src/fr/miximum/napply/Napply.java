1b274c26170e58131c78a8cf6ca854d3182109cb
Thibault Jouannic <teeboo@gmail.com>
2011-02-25 12:42:25 +0100

    napply is no longer an activity

--- Napply/src/fr/miximum/napply/Napply.java
+++ Napply/src/fr/miximum/napply/Napply.java
@@ -1,9 +1,6 @@
 package fr.miximum.napply;
 
-import android.app.Activity;
-import android.os.Bundle;
-
-public class Napply extends Activity
+public class Napply
 {
     public static final String TAG = "Napply";
 
@@ -16,12 +13,4 @@ public class Napply extends Activity
     public static final String ACTION_SNOOZE_ALARM = "fr.miximum.napply.SNOOZE_ALARM";
 
     public static final String ALARM_TERMINATED = "fr.miximum.napply.ALARM_TERMINATED";
-
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
 }

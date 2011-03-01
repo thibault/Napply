05a78075c42c034e56c791f8eb6b7df70d98cd6d
Thibault Jouannic <teeboo@gmail.com>
2011-03-01 15:53:30 +0100

    about page on install

---
+++ Napply/src/fr/miximum/napply/About.java
@@ -0,0 +1,13 @@
+package fr.miximum.napply;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.about);
+    }
+}
